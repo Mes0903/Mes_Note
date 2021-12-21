fc886c2971e0bcc255145f901cfbcea1bdc2d711
Mes <sandstorm0903@gmail.com>
2021-12-22 00:39:26 +0800

    12/22 Add Cpp Implement

--- The_mind_behind_Linux_Note/Elegant_Linked_List.cpp
+++ The_mind_behind_Linux_Note/Elegant_Linked_List.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <memory>
 
-struct Node {
-    int data;
-    std::unique_ptr<Node> next;
+class List {
+    struct Node {
+        int data;
+        Node *next;
+    };
+
+    Node *head;
+    Node **find(Node *target);
+
+  public:
+    void erase(Node *target);
+    void ins
 };
-
-
-
-int main()
-{
-}
